6527b7bc5750c3dd3c5dbbbc81cd9960060ee753
isatishrajput <chauhansatish978@gmail.com>
2025-12-15 07:14:04 +0530

    Index.js only runs when database is UP

--- creating-course-selling-app/index.js
+++ creating-course-selling-app/index.js
@@ -2,24 +2,27 @@ import "../loadEnv.js";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import {UserRouter} from "./routes/user.js";
-import {CourseRouter} from "./routes/course.js";
-import {AdminRouter} from "./routes/admin.js";
+import { UserRouter } from "./routes/user.js";
+import { CourseRouter } from "./routes/course.js";
+import { AdminRouter } from "./routes/admin.js";
 const PORT = process.env.PORT;
 const URI = process.env.MONGO_DB_CONNECTION_STRING;
 
-mongoose.connect(URI);
+// mongoose.connect(URI);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/user/",UserRouter);
-app.use("/course/",CourseRouter);
-app.use("/admin/",AdminRouter);
+app.use("/user/", UserRouter);
+app.use("/course/", CourseRouter);
+app.use("/admin/", AdminRouter);
 
-app.listen(PORT, (req, res) => {
+async function main() {
+  await mongoose.connect(URI);
+  app.listen(PORT);
   console.log(`Server is Running on PORT ${PORT}`);
-});
+}
 
+main();
 // p5.js-web-editor
